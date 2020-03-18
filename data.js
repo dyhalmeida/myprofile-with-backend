45d0a3e94ea5bedbdcde597b6106258fdc783124
Diego Almeida <dyhalmeida@gmail.com>
2020-03-18 16:21:56 -0300

    dados dinâmicos

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id:"Fbu7z5dXcRs",
+    title:"SQL no Node.JS com Sequelize",
+    duration:"30 min",
+    price: "Free"
+  },
+  {
+    id:"ICIz5dE3Xfg",
+    title:"Deploy de Apps Node.JS",
+    duration:"74 min",
+    price: "Free"
+  },
+  {
+    id:"3tB0uDliS6Y",
+    title:"Masterizando o Insomnia",
+    duration:"34 min",
+    price: "Free"
+  },
+  {
+    id:"VpeU3VpszAc",
+    title:"Aliases no GIT",
+    duration:"30 min",
+    price: "Free"
+  },
+  {
+    id:"p6d3RzhgQbA",
+    title:"Criando Templates",
+    duration:"07 min",
+    price: "Free"
+  },
+  {
+    id:"0mYq5LrQN1s",
+    title:"Typescript, o início de forma prática",
+    duration:"70 min",
+    price: "Free"
+  },
+]
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 
 // Configuração para servir arquivos estáticos
 server.use(express.static('public'));
@@ -9,11 +10,30 @@ server.use(express.static('public'));
 // Configuração da template engine
 server.set('view engine', 'njk');
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false,
+  noCache: true
 });
 
 
-server.get('/', (req, res) => res.render('about'));
-server.get('/lessons', (req, res) => res.render('lessons'));
+server.get('/', (req, res) => { 
+  const about = {
+    avatar_url: 'https://media-exp1.licdn.com/dms/image/C4E03AQEcXPVXyi6dSQ/profile-displayphoto-shrink_200_200/0?e=1589414400&v=beta&t=gvU2AI42JiMYKdNSSJ8AhwLECAzMjnmFixHsdGqhk9A',
+    name: 'Diego Almeida',
+    role: 'Javascript Programmer',
+    description: 'Work at <a href="http://grupocata.com.br/" target="_blank" title="Work at CATA">CATA</a>',
+    links: [
+      {
+        name: "Github", url: "https://github.com/dyhalmeida"
+      },
+      {
+        name: "Linkedin", url: "https://www.linkedin.com/in/dyhalmeida/"
+      }
+    ]
+  }
+  res.render('about', { ...about }); 
+});
+
+server.get('/lessons', (req, res) => res.render('lessons', { items: videos }));
 
 server.listen(5000, () => console.log('server is running'));
