10ecf7c5a840efc2cf522f6ee4a62dfc67c3ed79
Diego Almeida <dyhalmeida@gmail.com>
2020-03-18 09:57:31 -0300

    configuração do template engine nunjucks

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
-server.get('/', (req, res) => res.send('Server is running'));
+// Configuração da template engine
+server.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: server
+});
+
+
+server.get('/', (req, res) => res.render('index'));
 
 server.listen(5000, () => console.log('server is running'));
