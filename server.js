f418133f130ce223008509ca3a518844cc66d949
Diego Almeida <dyhalmeida@gmail.com>
2020-03-18 09:47:47 -0300

    configuração do nodemon + express server http

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  return res.send('Server is runnig');
+  return res.send('Server is running');
 });
 
 server.listen(5000, () => {
-  console.log('server is runnig');
+  console.log('server is running');
 });
