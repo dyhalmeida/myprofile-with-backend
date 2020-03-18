072ecd467c2bcd0c814a7d16015c29566c9b44be
Diego Almeida <dyhalmeida@gmail.com>
2020-03-18 10:46:19 -0300

    configuração de arquivos .njk

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.static('public'));
 
 // Configuração da template engine
-server.set('view engine', 'html');
+server.set('view engine', 'njk');
 nunjucks.configure('views', {
   express: server
 });
